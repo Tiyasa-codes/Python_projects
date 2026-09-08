6434251f581e04b90ccb59499a3f4b5816de4c62
Tiyasa <t90218900@gmail.com>
2026-09-09 00:53:16 +0530

    Improved password validator

--- v1.py
+++ v1.py
@@ -1,39 +1,29 @@
 text=input("Enter your password:")
-flag=0
-flag2=0
-flag3=0
-flag4=0
+has_upper_case=0
+has_lower_case=0
+has_digit=0
+has_special_Char=0
 for character in text:
     if(character.isupper()):
-        flag=1
+        has_upper_case=1
     if(character.islower()):
-        flag2=1
+        has_lower_case=1
     if(character.isdigit()):
-        flag3=1
+        has_digit=1
     if(not character.isalnum()):
-        flag4=1
-if(flag==1 and flag2==1 and flag3==1 and len(text)>=8 and flag4==1):
+        has_special_Char=1
+if(has_upper_case==1 and has_lower_case==1 and has_digit==1 and len(text)>=8 and has_special_Char==1):
     print("✅Strong Password!")
 else:
     print("Weak Password❌")
     print("Missing:")
-    if(flag==0):
+    if(has_upper_case==0):
      print("❌Add an uppercase letter!!")
-    if(flag2==0 ):
+    if(has_lower_case==0 ):
      print("❌Add an lowercase letter!!")
-    if(flag3==0):
+    if(has_digit==0):
      print("❌Add an digit!!")
     if( len(text)<8):
      print("❌Add more characters upto 8!!")
-    if(flag4==0 ):
+    if(has_special_Char==0 ):
      print("❌Add an special character!!")
-
-
-# text = input("Enter your password: ")
-
-# for character in text:
-#     if character.isupper():
-#         print("True")
-#         break
-# else:
-#     print("False")
