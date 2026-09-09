fc034ee1315207ced8209e87361c3db26fb1122a
Tiyasa <t90218900@gmail.com>
2026-09-09 10:49:04 +0530

    Improve password checker with boolean flags

--- v1.py
+++ v1.py
@@ -1,29 +1,29 @@
 text=input("Enter your password:")
-has_upper_case=0
-has_lower_case=0
-has_digit=0
-has_special_Char=0
+has_upper_case=False
+has_lower_case=False
+has_digit=False
+has_special_Char=False
 for character in text:
     if(character.isupper()):
-        has_upper_case=1
+        has_upper_case=True
     if(character.islower()):
-        has_lower_case=1
+        has_lower_case=True
     if(character.isdigit()):
-        has_digit=1
+        has_digit=True
     if(not character.isalnum()):
-        has_special_Char=1
-if(has_upper_case==1 and has_lower_case==1 and has_digit==1 and len(text)>=8 and has_special_Char==1):
+        has_special_Char=True
+if(has_upper_case and has_lower_case and has_digit and len(text)>=8 and has_special_Char):
     print("✅Strong Password!")
 else:
     print("Weak Password❌")
     print("Missing:")
-    if(has_upper_case==0):
+    if(not has_upper_case):
      print("❌Add an uppercase letter!!")
-    if(has_lower_case==0 ):
+    if(not has_lower_case):
      print("❌Add an lowercase letter!!")
-    if(has_digit==0):
+    if(not has_digit):
      print("❌Add an digit!!")
     if( len(text)<8):
      print("❌Add more characters upto 8!!")
-    if(has_special_Char==0 ):
+    if(not has_special_Char):
      print("❌Add an special character!!")
